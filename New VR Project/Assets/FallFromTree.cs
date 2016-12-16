6b8e012605cc81e0b1f5c56869758f0b703902cc
Reuben K. Coutinho <reuben.coutinho@accenture.com>
2016-12-16 12:34:09 +1000

    Chore: Added floating animation to coconut via C# script

--- New VR Project/Assets/FallFromTree.cs
+++ New VR Project/Assets/FallFromTree.cs
@@ -11,9 +11,6 @@ public class FallFromTree : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (transform.position.y > 0.6f)
-        {
-            transform.Translate(0, -2.5f * Time.deltaTime, 0, Space.World);
-        }
+        transform.position = new Vector3(0, 5 + Mathf.Sin(Time.time * 5.0f), 0);
     }
 }
