8c8fe44f1f32a917e77434331946743e63dbb6cc
Reuben Coutinho <reuben.coutinho@gmail.com>
2016-12-17 19:31:26 +1000

    Chore: created a new scene to learn in. Created a new C# script that will create objects. Imported Google VR SDK v1.10

---
+++ New VR Project/Assets/VRND-Course3-Course_Assets/Scripts/Learning/ObjectMaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMaker : MonoBehaviour {
+
+	public GameObject insertGameObjetHere;
+
+	// Use this for initialization
+	void Start () {
+		//make object here
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
