c60eaf17ee9858ccf2e3b775208a847fa4a78af1
Reuben Coutinho <reuben.coutinho@gmail.com>
2016-12-18 08:19:04 +1000

    Refactor: added a new class to create many objects. Reverted ObjectMaker class to original format

--- New VR Project/Assets/VRND-Course3-Course_Assets/Scripts/Learning/ManyObjectMaker.cs
+++ New VR Project/Assets/VRND-Course3-Course_Assets/Scripts/Learning/ManyObjectMaker.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMaker : MonoBehaviour {
+public class ManyObjectMaker : MonoBehaviour {
 
 	public GameObject objectToCreate;
 
 	// Use this for initialization
 	void Start () {
 		//make object here
-		Object.Instantiate(objectToCreate, new Vector3(2, 4, 6), Quaternion.identity);
+		for (int i = 0; i < 1000; i++) {
+			Object.Instantiate(objectToCreate, new Vector3(i, 4, 6), Quaternion.identity);
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
