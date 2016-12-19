a8d5dc90e3ef915099a8ec3cbf4ba33bdb4ff847
Reuben K. Coutinho <reuben.coutinho@accenture.com>
2016-12-19 16:21:26 +1000

    Refactor: updated code to trigger sun animation

--- New VR Project/Assets/VRND-Course3-Course_Assets/StarterContent/RotateLight.cs
+++ New VR Project/Assets/VRND-Course3-Course_Assets/StarterContent/RotateLight.cs
@@ -6,16 +6,30 @@ public class RotateLight : MonoBehaviour {
 
     public GameObject directionalLight;
 
-	// Use this for initialization
-	void Start () {
-		
+    float startTime = 0f;
+    bool isPressed = false;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        if (GvrViewer.Instance.Triggered && !isPressed)
+            ActivateRotation();
+
         Quaternion startRotation = Quaternion.Euler(50f, 30f, 0);
         Quaternion endRotation = Quaternion.Euler(200f, 30f, 0);
-        directionalLight.transform.rotation = Quaternion.Slerp(startRotation, endRotation, Time.time / 20f);
+        if (isPressed)
+        {
+            directionalLight.transform.rotation = Quaternion.Slerp(startRotation, endRotation, startTime / 20f);
+            startTime += Time.deltaTime;
+        }
+    }
+
+    public void ActivateRotation() {
+        isPressed = true;
     }
 }
