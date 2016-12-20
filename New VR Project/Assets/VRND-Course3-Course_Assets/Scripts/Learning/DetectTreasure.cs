b92093560af6fbe3998b01807596b15e96283e45
Reuben K. Coutinho <reuben.coutinho@accenture.com>
2016-12-20 11:12:33 +1000

    Refactor: Updated animation to trigger on detect via code

--- New VR Project/Assets/VRND-Course3-Course_Assets/Scripts/Learning/DetectTreasure.cs
+++ New VR Project/Assets/VRND-Course3-Course_Assets/Scripts/Learning/DetectTreasure.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DetectTreasure : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Animator openChest;
+    private bool watched = false;
+    // Use this for initialization
+    void Start ()
+    {
+        openChest.StartPlayback();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,9 +21,18 @@ public class DetectTreasure : MonoBehaviour {
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(transform.position, fwd, 10))
-            print("There is something in front of the object!");
-        else
-            print("There is nothing in front of the object!");
+        if (Physics.Raycast(transform.position, fwd, 10) && !watched)
+            ActivateChestAnimation();
+
+        if (watched)
+        {
+            openChest.StopPlayback();
+        }
+    }
+
+    public void ActivateChestAnimation()
+    {
+        watched = true;
+        openChest.SetBool("OpenLid", true);
     }
 }
